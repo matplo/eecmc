efdf9d3e4133a17b8ab7f0dacb73f5ff373723cd
matplo <ploskon@gmail.com>
2024-06-14 21:55:17 -0700

    scale integral; gen

--- pythia8/pythia_analysis.sh
+++ pythia8/pythia_analysis.sh
@@ -6,4 +6,10 @@
 
 # ../analysis/simple_jet_rec.py --enable-eec --ncounts 1000 --nev 1000 --D0mode 1 --py-hardQCDcharm --py-pthatmin 15 --jet-pt-min 15 --jet-pt-max 30 --D0-pt-min 5 --D0-pt-max 30 $@
 
-../analysis/simple_jet_rec.py --enable-eec --ncounts 1000 --nev -1 --D0mode 1 --py-hardQCDcharm --py-pthatmin 15 --jet-pt-min 15 --jet-pt-max 30 --D0-pt-min 5 --D0-pt-max 30 --nev 100000 --py-charm-mass 0 --output pythia_charm0.root
+../analysis/simple_jet_rec.py --enable-eec --ncounts 10000 --nev -1 --D0mode 1 --py-hardQCDcharm --py-pthatmin 15 --jet-pt-min 15 --jet-pt-max 30 --D0-pt-min 5 --D0-pt-max 30 --nev -1 --py-charm-mass 0 --output pythia_charm0.root &
+
+../analysis/simple_jet_rec.py --enable-eec --ncounts 10000 --nev -1 --D0mode 1 --py-hardQCDcharm --py-pthatmin 15 --jet-pt-min 15 --jet-pt-max 30 --D0-pt-min 5 --D0-pt-max 30 --nev -1 --py-charm-mass 2.84 --output pythia_charm2.root &
+
+../analysis/simple_jet_rec.py --enable-eec --ncounts 10000 --nev -1 --D0mode 1 --py-hardQCDcharm --py-pthatmin 15 --jet-pt-min 15 --jet-pt-max 30 --D0-pt-min 5 --D0-pt-max 30 --nev -1 --output pythia_charmSM.root &
+
+# ../analysis/simple_jet_rec.py --enable-eec --ncounts 10000 --nev -1 --py-hardQCDuds --py-pthatmin 15 --jet-pt-min 15 --jet-pt-max 30 --nev -1 --output pythia_lfq.root
