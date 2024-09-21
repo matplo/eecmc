3921e6e2638753778f57c5c032fe8f53abbf4e0f
matplo <ploskon@gmail.com>
2024-09-21 05:36:42 +0000

    stderr

--- pythia8/run_pcone_bg.sh
+++ pythia8/run_pcone_bg.sh
@@ -4,8 +4,8 @@
 
 nev=100000
 commands=()
-commands+=("./pythia8_simple_eec.py --py-cmnd pythia_pp_5TeV.cmnd --jet-pt-min 20. --charged --py-pthatmin 20. --py-seed 123456 -o pcone_bg_pp.root --nev ${nev} | tee pcone_bg_pp.log")
-commands+=("./pythia8_simple_eec.py --py-cmnd pythia_pPb_5TeV_nPDF_argantyr.cmnd --jet-pt-min 20. --charged --py-pthatmin 20. --py-seed 123456 -o pcone_bg_pPb_argantyr.root --nev ${nev} | tee pcone_bg_pPb_argantyr.log")
+commands+=("./pythia8_simple_eec.py --py-cmnd pythia_pp_5TeV.cmnd --jet-pt-min 20. --charged --py-pthatmin 20. --py-seed 123456 -o pcone_bg_pp.root --nev ${nev} 2>&1 | tee pcone_bg_pp.log")
+commands+=("./pythia8_simple_eec.py --py-cmnd pythia_pPb_5TeV_nPDF_argantyr.cmnd --jet-pt-min 20. --charged --py-pthatmin 20. --py-seed 123456 -o pcone_bg_pPb_argantyr.root --nev ${nev} 2>&1 | tee pcone_bg_pPb_argantyr.log")
 
 nproc=$(get_arg "--nproc" $@)
 if [ -z "${nproc}" ]; then
